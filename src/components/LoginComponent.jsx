15ab04ed04609a472047fa0d249906f53758eded
juanxbini <juanxbini@gmail.com>
2025-03-03 16:57:28 -0300

    componente listo, con manejadores y dispatch listos, aun sin los actions vinculados

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,35 +1,52 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useState } from 'react'
 
 const LoginComponent = () => {
-  return (
-    <div>
-        <h2>Autenticación</h2>
-        {loading && <p>Cargando...</p>}
-        {error && <p>{error}</p>}
-        {isAuthenticated ? (
-            <>
-                <p>Usuario autenticado</p>
-                <button onClick={logout}>Cerrar sesión</button>
-            </>
-        ) : (
-            <>
-                <input
-                    type="text"
-                    placeholder="Usuario"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button onClick={login}>Iniciar sesión</button>
-            </>
-        )}
-    </div>
-  )
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const dispatch = useDispatch()
+    const { user, loading, error, isAuthenticated } = useSelector((state) => state.auth)
+
+
+    const handleLogin = () => {
+        dispatch({ type: 'LOGIN_REQUEST', payload: { email, password } })
+    }
+
+    const handleLogout = () => {
+        dispatch({ type: 'LOGOUT_REQUEST' })
+    }
+
+
+    return (
+        <div>
+            <h2>Autenticación</h2>
+            {loading && <p>Cargando...</p>}
+            {error && <p>{error}</p>}
+            {isAuthenticated ? (
+                <>
+                    <p>Usuario {user.name} autenticado</p>
+                    <button onClick={handleLogout}>Cerrar sesión</button>
+                </>
+            ) : (
+                <>
+                    <input
+                        type="text"
+                        placeholder="Usuario"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Contraseña"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button onClick={handleLogin}>Iniciar sesión</button>
+                </>
+            )}
+        </div>
+    )
 }
 
 export default LoginComponent
